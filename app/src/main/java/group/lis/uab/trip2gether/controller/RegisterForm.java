8b017c8464728cb855d7808f016e37652404f82c
jeff039 <jofreorus@gmail.com>
2015-03-24 19:58:55 +0100

    Missatge d'error si no es mail de registre és repetit

--- app/src/main/java/group/lis/uab/trip2gether/controller/RegisterForm.java
+++ app/src/main/java/group/lis/uab/trip2gether/controller/RegisterForm.java
@@ -3,17 +3,23 @@ package group.lis.uab.trip2gether.controller;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.parse.ParseCloud;
 import com.parse.ParseException;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Formatter;
 import java.util.HashMap;
 
 import group.lis.uab.trip2gether.R;
@@ -40,14 +46,16 @@ public class RegisterForm extends ActionBarActivity {
             try {
                 boolean register = RegisterForm.this.register(RegisterForm.this.getName(),
                         RegisterForm.this.getSurname(),RegisterForm.this.getCity(),
-                        RegisterForm.this.getMail(),RegisterForm.this.getPassword());
+                        RegisterForm.this.getMail(),RegisterForm.this.getPassword(),
+                        RegisterForm.this.getCountry());
 
             if(register) {
+                Toast.makeText(getApplicationContext(), "Registration completed", Toast.LENGTH_SHORT).show();
                 Intent mainLaunchLogin = new Intent(RegisterForm.this, MainLaunchLogin.class);
                 startActivity(mainLaunchLogin);
             }
             else {
-                //TODO
+                Toast.makeText(getApplicationContext(), "Error registering the user", Toast.LENGTH_SHORT).show();
             }
         } catch (ParseException e) {
             e.printStackTrace();
@@ -56,14 +64,16 @@ public class RegisterForm extends ActionBarActivity {
     };
 
     ////////////////REGISTER////////////////////////////////////////
-    public boolean register(String name, String surname, String city, String mail, String password) throws ParseException {
+    public boolean register(String name, String surname, String city, String mail, String password, String country) throws ParseException {
         boolean success = false;
+        password = RegisterForm.encryptPassword(password);
         HashMap<String, Object> params = new HashMap<String, Object>();
         params.put("name", name);
         params.put("surname", surname);
         params.put("city", city);
         params.put("mail", mail);
         params.put("password", password);
+        params.put("country", country);
 
         ArrayList checkRegistro = ParseCloud.callFunction("checkUserSignIn", params);
 
@@ -71,10 +81,10 @@ public class RegisterForm extends ActionBarActivity {
             return success;
         }
         else {
-            ArrayList registerResponse = ParseCloud.callFunction("register", params); //crida al BE
-            if (registerResponse.size() == 1) //tenim un usuari
+            String registerResponse = ParseCloud.callFunction("register", params); //crida al BE
+            if(registerResponse.isEmpty() == false) //S'ha creat l'usuari
                 success = true;
-            System.out.println(success);
+            Log.i("Registration objectId:", registerResponse);
             return success;
         }
     }
@@ -113,6 +123,45 @@ public class RegisterForm extends ActionBarActivity {
         EditText password = (EditText)findViewById(R.id.password);
         String passwordText = password.getText().toString();
         return passwordText;
+    }
+
+    public String getCountry() {
+        EditText country = (EditText) findViewById(R.id.country);
+        String countryText = country.getText().toString();
+        return countryText;
+    }
+
+    private static String encryptPassword(String password)
+    {
+        String sha1 = "";
+        try
+        {
+            MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+            crypt.reset();
+            crypt.update(password.getBytes("UTF-8"));
+            sha1 = byteToHex(crypt.digest());
+        }
+        catch(NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        catch(UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        }
+        return sha1;
+    }
+
+    private static String byteToHex(final byte[] hash)
+    {
+        Formatter formatter = new Formatter();
+        for (byte b : hash)
+        {
+            formatter.format("%02x", b);
+        }
+        String result = formatter.toString();
+        formatter.close();
+        return result;
     }
 
     ////////////////////////BARRA SUPERIOR////////////////////////////////////
